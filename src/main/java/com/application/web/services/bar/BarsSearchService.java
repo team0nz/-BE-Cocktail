f8412a174a79410c0105d020de3516638e87da99
c0smosaur <sjkang539@gmail.com>
2025-02-28 01:31:09 +0900

    #5 FIX: DTO 변환 로직을 Response 클래스로 이동

--- src/main/java/com/application/web/services/bar/BarsSearchService.java
+++ src/main/java/com/application/web/services/bar/BarsSearchService.java
@@ -3,15 +3,12 @@ package com.application.web.services.bar;
 import com.application.common.exception.custom.EntityNotFoundException;
 import com.application.domain.bar.entity.Bar;
 import com.application.domain.bar.model.response.BarDetailResponse;
-import com.application.domain.bar.model.response.BarResponse;
 import com.application.domain.bar.model.response.BarSearchResponse;
 import com.application.domain.bar.repository.BarRepository;
 import com.application.domain.bar.repository.BarSearchRepository;
 import com.application.domain.barImage.entity.BarImage;
-import com.application.domain.barImage.model.BarImageResponse;
 import com.application.domain.barImage.repository.BarImageRepository;
 import com.application.domain.menu.entity.Menu;
-import com.application.domain.menu.model.MenuResponse;
 import com.application.domain.menu.repository.MenuRepository;
 import com.application.domain.openHour.entity.OpenHour;
 import com.application.domain.openHour.repository.OpenHourRepository;
@@ -20,7 +17,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,28 +39,21 @@ public class BarsSearchService {
         return BarDetailResponse.from(bar, menus, openHours, barImages);
     }
 
-    public List<BarSearchResponse> getBarsByBarNameOrMenuName(String keyword, Pageable pageable) {
+    public Page<BarSearchResponse> getBarsByBarNameOrMenuName(String keyword, Pageable pageable) {
         return getBarSearchResponses(searchBarsByBarNameOrMenuName(keyword, pageable));
     }
 
-    public List<BarSearchResponse> getBarsByCurrentCoordinates(double currentX, double currentY, Pageable pageable) {
+    public Page<BarSearchResponse> getBarsByCurrentCoordinates(double currentX, double currentY, Pageable pageable) {
         return getBarSearchResponses(searchBarsByCurrentCoordinates(currentX, currentY, pageable));
     }
 
-    public List<BarSearchResponse> getBarSearchResponses(Page<Bar> bars) {
-        List<BarSearchResponse> searchResponses = new ArrayList<>();
-
-        for (Bar bar : bars) {
-            BarResponse barResponse = BarResponse.from(bar);
+    public Page<BarSearchResponse> getBarSearchResponses(Page<Bar> bars) {
+        return bars.map(bar -> {
             List<Menu> menus = menuRepository.findAllByBar(bar);
-            List<MenuResponse> menuResponses = menus.stream().map(MenuResponse::from).toList();
             BarImage barImage = barImageRepository.findFirstByBar(bar);
-            BarImageResponse img = BarImageResponse.from(barImage);
 
-            searchResponses.add(new BarSearchResponse(barResponse, menuResponses, img));
-        }
-
-        return searchResponses;
+            return BarSearchResponse.from(bar, menus, barImage);
+        });
     }
 
     private Page<Bar> searchBarsByBarNameOrMenuName(String keyword, Pageable pageable) {
