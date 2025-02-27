34059cb3d797bbfac6447acfc8b48166447b860f
c0smosaur <sjkang539@gmail.com>
2025-02-28 01:29:52 +0900

    #5 FIX: List에서 Page를 반환하도록 변환

--- src/main/java/com/application/domain/bar/model/response/BarSearchResponse.java
+++ src/main/java/com/application/domain/bar/model/response/BarSearchResponse.java
@@ -1,6 +1,9 @@
 package com.application.domain.bar.model.response;
 
+import com.application.domain.bar.entity.Bar;
+import com.application.domain.barImage.entity.BarImage;
 import com.application.domain.barImage.model.BarImageResponse;
+import com.application.domain.menu.entity.Menu;
 import com.application.domain.menu.model.MenuResponse;
 
 import java.util.List;
@@ -8,5 +11,12 @@ import java.util.List;
 public record BarSearchResponse(BarResponse bar,
                                 List<MenuResponse> menus,
                                 BarImageResponse barImage) {
+
+    public static BarSearchResponse from(Bar bar, List<Menu> menus, BarImage barImage) {
+        return new BarSearchResponse(BarResponse.from(bar),
+                menus.stream().map(MenuResponse::from).toList(),
+                BarImageResponse.from(barImage)
+                );
+    }
 
 }
